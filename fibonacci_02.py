d3a3ad0b1b5e530163160c3be94d88417a7e2ff3
Rafael Moctezuma <moctezumarafael26@gmail.com>
2023-04-04 22:57:56 -0600

    Adicion de informacion faltante

--- fibonacci_02.py
+++ fibonacci_02.py
@@ -1,3 +1,14 @@
+'''
+ * Reto #2
+ * LA SUCESIÓN DE FIBONACCI
+ * Fecha publicación enunciado: 10/01/22
+ * Fecha publicación resolución: 04/04/23
+ * Dificultad: DIFÍCIL
+ *
+ * Enunciado: Escribe un programa que imprima los 50 primeros números de la sucesión de Fibonacci empezando en 0.
+ * La serie Fibonacci se compone por una sucesión de números en la que el siguiente siempre es la suma de los dos anteriores.
+ * 0, 1, 1, 2, 3, 5, 8, 13...
+'''
 def fibonacci(num):
     if num == 0:
         return 0;
