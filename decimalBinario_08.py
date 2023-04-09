b09a2b9c9605163b74a4cfa7db98dafde622f744
Rafael Moctezuma <moctezumarafael26@gmail.com>
2023-04-09 17:09:42 -0600

    Noveno Reto - Decimal a Binario

---
+++ decimalBinario_08.py
@@ -0,0 +1,17 @@
+'''
+ * Reto #8
+ * DECIMAL A BINARIO
+ * Fecha publicación enunciado: 18/02/22
+ * Fecha publicación resolución: 02/03/22
+ * Dificultad: FÁCIL
+ *
+ * Enunciado: Crea un programa se encargue de transformar un número decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+'''
+
+def binario(num):
+    binary = "";
+    while num != 0:
+        num_binario = num % 2;
+        num = int(num/2);
+        binary = str(num_binario) + binary;
+    return binary;
